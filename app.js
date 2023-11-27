8adc98c60bcbe8125394aa9ead8073aee54461c2
arturir <artifakt666@yandex.ru>
2023-11-27 23:10:20 +0300

    bugfix: исправить ошибки после автоестов

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {});
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
+express.use((req, res, next) => {
+  req.user = {
+    _id: "655e69f68f7be2536861c904",
+  };
+  next();
+});
 app.use(require("./routes/index"));
 
 app.listen(PORT, () => {

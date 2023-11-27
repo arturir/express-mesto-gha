ad34c7718c9fe5883120512a08ca722b49cfeb20
arturir <artifakt666@yandex.ru>
2023-11-27 23:13:05 +0300

    bugfix: исправить ошибки app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {});
 const { PORT = 3000 } = process.env;
 
 const app = express();
-express.use((req, res, next) => {
+app.use((req, res, next) => {
   req.user = {
     _id: "655e69f68f7be2536861c904",
   };
